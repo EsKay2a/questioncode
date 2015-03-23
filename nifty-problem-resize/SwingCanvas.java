5a2a01034b243f24575535e22b91efa21d9e5341
EsKay <kreide@gmx.de>
2015-03-23 21:45:44 +0100

    Create SwingCanvas.java

---
+++ nifty-problem-resize/SwingCanvas.java
@@ -0,0 +1,181 @@
+import com.jme3.niftygui.NiftyJmeDisplay;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+...
+
+public class SwingCanvas extends SimpleApplication implements ScreenController {
+       
+    private Nifty nifty;
+    ...
+	
+    @Override
+    public void simpleInitApp() {
+       
+ //========================= NIFTY========================================           
+        NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager,
+                                                  inputManager,
+                                                  audioRenderer,
+                                                  guiViewPort);
+        nifty = niftyDisplay.getNifty();
+        
+        nifty.fromXml("Interface/test.xml", "hud", this);
+        nifty.setDebugOptionPanelColors(true); // debug colors enabled       
+        
+        // attach the nifty display to the gui view port as a processor
+        guiViewPort.addProcessor(niftyDisplay); 
+   
+
+//========================= NIFTY======================================== 
+
+        inputManager.setCursorVisible(true);       
+        initKeys();            
+        initMyFlyByCam(); 
+        setCamStart();
+        initGrid();
+        initScene();
+        initInfoDisplay();
+        initLight();
+	
+    }
+    
+    @Override
+    public void bind(Nifty nifty, Screen screen) {
+        System.out.println("bind( " + screen.getScreenId() + ")");
+    }
+
+    @Override
+    public void onStartScreen() {
+        System.out.println("onStartScreen");
+    }
+
+    @Override
+    public void onEndScreen() {
+        System.out.println("onEndScreen");
+    }
+
+    public void quit(){
+        nifty.gotoScreen("end");
+    }
+    
+    
+    private void initScene(){ 
+		// some OpenGL stuff  
+	}
+
+   
+        
+       int hight =180;
+       Rectangle textBox = new Rectangle(0, 0, 200, 200);
+               
+                   
+        Material matd = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        //wichtig mit assetManager funktioniert es nicht >> Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md")
+        matd.setColor("Color", new ColorRGBA(0.3f,0.3f,0.3f,0.6f));
+        matd.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
+
+        
+        Quad d= new Quad(200,200);
+        Geometry display = new Geometry("Display", d);
+        display.setMaterial(matd);
+        display.setLocalTranslation(0, hight, 0);
+
+        infoPanelNode.attachChild(display);
+          
+        hight= (int)d.getHeight();
+                
+        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+        BitmapText textNode = new BitmapText(guiFont, true);
+        textNode.setSize(guiFont.getCharSet().getRenderedSize()-5);
+        textNode.setText("Modelrotation (L): " +modelNode.getLocalRotation());
+        infoPanelNode.attachChild(textNode);
+        hight+=textNode.getLineHeight();
+        textNode.setLocalTranslation(0, hight, 0);
+        
+        textNode.setBox(textBox);
+        textNode.setColor(ColorRGBA.White);
+
+        
+        BitmapText infoLocalRotation = new BitmapText(guiFont, false);
+        BitmapText infoCamPosition = new BitmapText(guiFont, false);
+
+        
+        infoLocalRotation.setSize(guiFont.getCharSet().getRenderedSize()-3);    // font size  
+        infoCamPosition.setSize(guiFont.getCharSet().getRenderedSize()-3);
+        
+        infoLocalRotation.setColor(ColorRGBA.Blue); 
+        
+        //infoLocalRotation.setText("Modelrotation: " +modelNode.getLocalRotation()); //modelNode.getLocalRotation()
+        infoCamPosition.setText("Kameraposition" +cam.getLocation());
+        
+        hight+=infoLocalRotation.getLineHeight();
+        infoLocalRotation.setLocalTranslation(0, hight, 0);
+        hight+= infoCamPosition.getLineHeight();
+        infoCamPosition.setLocalTranslation(0, hight, 0);
+        infoPanelNode.attachChild(infoLocalRotation);  
+        infoPanelNode.attachChild(infoCamPosition); 
+        
+    }
+   
+    public JmeCanvasContext createJMEcanvas(){
+        int frameRate=60;
+        /** Canvas application settings */                 
+        settings = new AppSettings(true);
+        settings.setRenderer(AppSettings.LWJGL_OPENGL_ANY);
+        settings.setFrameRate(frameRate); // Framerate  
+        settings.setWidth(640);
+        settings.setHeight(480);
+        this.setSettings(settings);
+
+        this.createCanvas();
+        
+        JmeCanvasContext ctx = (JmeCanvasContext) this.getContext();
+        ctx.setSystemListener(this);
+        Dimension dim = new Dimension(640, 480);              
+        ctx.getCanvas().setPreferredSize(dim);
+        return ctx;
+    }    
+     
+public static void main(String[] args) {
+     java.awt.EventQueue.invokeLater(new Runnable() {    
+         //createCanvas();
+         @Override
+        
+         public void run() {             
+             
+             JPanel canvasPanel = new JPanel (new BorderLayout()); //main panel
+             
+			 SwingCanvas canvasApplication = new SwingCanvas(); 
+             JmeCanvasContext ctx = (JmeCanvasContext) canvasApplication.createJMEcanvas();            
+             //canvasApplication.setPauseOnLostFocus(false); //nifty
+             
+             JFrame window = new JFrame("HuSki Editor");                    
+             window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+           
+             //create optionPanel (class optionDisplay)          
+             optionDisplay optionPanel = new optionDisplay();
+             
+             // put all together           
+             window.add(ctx.getCanvas()); //adding CanvasApp (canvasApplication) to Swing frame (window)
+             canvasPanel.add(ctx.getCanvas(), BorderLayout.CENTER); // adding CanvasApp (canvasApplication) to BorderLayout position (JPanel)
+             canvasPanel.add(optionPanel, BorderLayout.EAST); // adding optionPanel to BorderLayout position (JPanel)
+             
+             window.add(canvasPanel);
+             window.pack();
+             window.setLocation(640/2, 480/2); // start position
+             //setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+             window.setVisible(true);           
+			 
+             canvasApplication.startCanvas(); 
+             
+         }
+     });
+ }
+
+    @Override
+    public void simpleUpdate(float tpf){
+        //write here update stuff
+		...
+    } 
+
+}
